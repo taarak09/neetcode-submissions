0bdf299dbf36c8e5743df3d91f035ce75dee5d1f
Taarak Gulhane <taarakgulhane09@gmail.com>
2026-06-11 12:48:21 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        //stack question
+        if(s=="")
+        {
+            return true;
+        }
+        stack<int>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);}
+            else{
+                if(!st.empty() && s[i]=='}')
+                {
+                    if(st.top()=='{')
+                    {
+                        st.pop();
+
+                    }else{
+                        return false;
+                    }
+                }
+                else if (!st.empty() &&   s[i]==']')
+                {
+                    if(st.top()=='[')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    if(  !st.empty() && st.top()=='(')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+
+       
+   return false; }
+};
