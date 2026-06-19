39fcca85b6764951c287b0e70e03e245a6079e8a
Taarak Gulhane <taarakgulhane09@gmail.com>
2026-06-19 16:36:37 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       //kadane's algorithm 
+       //prefix sum type;
+       int ans=INT_MIN;
+       int sum=0;
+       for(int i=0;i<nums.size();i++)
+       {
+        sum+=nums[i];
+        ans=max(ans,sum);
+        if(sum<0)
+        {
+            sum=0;
+        }
+       }
+   return ans; }
+};
