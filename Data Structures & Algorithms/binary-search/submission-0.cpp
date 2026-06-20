a71e20f9828e4582a334d10bbbdec3ddfe41f7ab
Taarak Gulhane <taarakgulhane09@gmail.com>
2026-06-20 16:41:35 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        if(it!=nums.end() && *it==target)
+        {
+            return it-nums.begin();
+        }
+        else{
+            return -1;
+        }
+    }
+};
