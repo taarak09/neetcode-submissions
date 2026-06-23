4062e680568347d95bd8e5ef274ef20403bc9f13
Taarak Gulhane <taarakgulhane09@gmail.com>
2026-06-23 17:17:44 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        for(int i=0;i<abs(n);i++)
+        {
+            ans*=x;
+        }
+        if(n<0)
+        {
+            ans=1/ans;
+        }
+        
+   return ans; }
+};
